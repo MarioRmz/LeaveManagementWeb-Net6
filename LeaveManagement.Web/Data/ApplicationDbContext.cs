58f41070aa536f24852253be660ab6d8258afce1
Mario Rmz <martrix75@hotmail.com>
2022-03-10 21:52:01 -0600

    Agregado de base de datos y migraciones. Agregado de tablas adicionales.

--- LeaveManagement.Web/Data/ApplicationDbContext.cs
+++ LeaveManagement.Web/Data/ApplicationDbContext.cs
@@ -3,11 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagement.Web.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    //public class ApplicationDbContext : IdentityDbContext
+    //Usa Employee para hacer la tabla de usuarios asp
+    public class ApplicationDbContext : IdentityDbContext<Employee>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
     }
 }
